f58c01670a9486719448271b0d8b6d2b26487a9d
Envaya <n.voegtli@gmx.ch>
2018-12-18 17:34:36 +0100

    introduced stackpane container for content

---
+++ src/main/java/ch/fhnw/oop2/skiareasfx/view/Content.java
@@ -0,0 +1,22 @@
+package ch.fhnw.oop2.skiareasfx.view;
+
+import ch.fhnw.oop2.skiareasfx.presentationmodel.RootPM;
+import javafx.geometry.Orientation;
+import javafx.scene.control.Button;
+import javafx.scene.control.SplitPane;
+import javafx.scene.layout.StackPane;
+
+public class Content extends StackPane implements ViewMixin {
+
+    public Content() {
+        init();
+    }
+
+    @Override
+    public void initializeControls() {
+    }
+
+    @Override
+    public void layoutControls() {
+    }
+}
